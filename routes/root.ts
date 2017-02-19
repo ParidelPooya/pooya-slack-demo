cef4a051a75355618daa57796211f6dcc0e6eb7a
Pooya Paridel <pooya.p@outlook.com>
2017-02-19 14:46:24 -0800

    Add - simple slack response

--- routes/root.ts
+++ routes/root.ts
@@ -1,23 +1,29 @@
 /**
  * Created by pooyaparidel on 2016-09-01.
  */
-(function () {
-    module.exports = function (app, status) {
+
+(function() {
+
+    module.exports = function(app,status) {
+
         app.get('/', function (req, res) {
-            let settings = require('../helper/settings');
+            let settings=require('../helper/settings');
+
             let data = {
-                response_type: 'in_channel',
+                response_type: 'in_channel', // public to the channel
                 text: '302: Found',
-                attachments: [
+                attachments:[
                     {
                         image_url: 'https://http.cat/302.jpg'
                     }
-                ] };
+                ]};
+
             res.setHeader('Content-Type', 'application/json');
             res.writeHead(200);
             res.write(JSON.stringify(data));
             res.end();
         });
     };
+
 }).call(this);
-//# sourceMappingURL=root.js.map
+
