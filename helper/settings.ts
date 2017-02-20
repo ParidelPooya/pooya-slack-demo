6fc0c0d59a367733e8d4674a3fec420ce9203665
Pooya Paridel <pooya.p@outlook.com>
2017-02-19 22:48:00 -0800

    Change - move some settings to env

--- helper/settings.ts
+++ helper/settings.ts
@@ -5,10 +5,7 @@
 (function() {
 
 var defSettings={
-    port:"5000",
-    SLACK_CLIENT_ID:"144193576743.143502641042",
-    SLACK_CLIENT_SECRET:"d14645f6a046c023b3e655f7bab38006",
-    SLACK_VERIFICATION_TOKEN:""
+    port:"5000"
 };
 
 module.exports = {
