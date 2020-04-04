2f05c50fc259798b6b36a4886086df2fd81c9a1d
Pooya Paridel <ParidelPooya@users.noreply.github.com>
2020-04-03 21:00:48 -0700

    Update root.js

--- routes/root.js
+++ routes/root.js
@@ -15,7 +15,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
             return __awaiter(this, void 0, void 0, function* () {
                 let data = {
                     "response_type": "ephemeral",
-                    "text": "Preparing..."
+                    "text": ""
                 };
 
                 res.setHeader('Content-Type', 'application/json');
